bbaf445e03c12b8b32df285675d1928561177244
Dorozco89 <dorozco89@unisalle.edu.co>
2026-03-20 14:12:07 -0500

    Clase sistema_inventario.py

---
+++ EmpresaTeleVentas/services/sistema_inventario.py
@@ -0,0 +1,6 @@
+class SistemaInventario:
+    def consultar_producto(self, codigo):
+        return "Producto consultado"
+
+    def actualizar_stock(self, producto, cantidad):
+        return "Stock actualizado"
