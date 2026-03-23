bf9cd30b973d498155705023901ae309fc8e608a
Dorozco89 <dorozco89@unisalle.edu.co>
2026-03-22 23:01:55 -0500

    Clase cesion.py

---
+++ Museo/models/cesion.py
@@ -0,0 +1,7 @@
+class Cesion:
+
+    def __init__(self, fecha_inicio, fecha_fin, importe, museo):
+        self._fecha_inicio = fecha_inicio
+        self._fecha_fin = fecha_fin
+        self._importe = importe
+        self._museo = museo
